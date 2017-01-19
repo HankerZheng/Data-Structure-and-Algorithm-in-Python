1644174bd92902c0a4d08032711eca1082d89a45
Hanker Zheng <zhenghan991@gmail.com>
2017-01-18 16:44:26 -0800

    HashHeap comments added

--- 018_HashHeap.py
+++ 018_HashHeap.py
@@ -1,6 +1,21 @@
+# !/usr/bin/python
+
+# This is the Python implementation of Hash Heap based on the list implementation 
+# of binary heap. The difference between Hash Heap and Binary Heap is that Hash
+# Heap supports the `heapRemove` operation in O(log n) time and can check whether
+# certain element is in the Hash Heap or not in O(1) time.
+# 
+# Basic automatic tests are given in `pushpopTest()` and `removeTest()`.
+# Note: It may takes about 10 seconds to run both test functions.
+
 import random
 
 class HeapNode(object):
+    """
+    The node in the HashHeap to deal with duplicates.
+    Each node store the value of each element and the number of duplicates
+    with the same value.
+    """
     def __init__(self, val, cnt):
         self.val = val
         self.cnt = cnt
@@ -13,7 +28,17 @@ class HeapNode(object):
     __repr__ = __str__
 
 class HashHeap(object):
+    """
+    This HashHeap is the same as the list implementation of binary heap, but with
+    a hashMap to map the value of one elemnt to its index in the list.
+    """
     def __init__(self, arr):
+        """
+        `_cap` - the number of elements in the HashHeap
+        `_maxIdx` - the max index of the binary heap
+        `_data` - the list implementation of the binary heap
+        `_hashMap` - mapping the element to its index in the binary heap
+        """
         elemCnt = self._preProcess(arr)
         self._cap = len(arr)
         self._maxIdx = len(elemCnt) - 1
@@ -22,12 +47,21 @@ class HashHeap(object):
         self._heapify()
 
     def _preProcess(self, arr):
+        """
+        Convert the input array into a dict object.
+        The key to the dict is the value of the element.
+        The value of the dict is the occurence of each element.
+        """
         elemCnt = {}
         for elem in arr:
             elemCnt[elem] = elemCnt.get(elem, 0) + 1
         return elemCnt
 
     def _swap(self, idx1, idx2):
+        """
+        Swap the 2 elements in the heap.
+        Also, change the index stored in `self._hashMap`
+        """
         elem1, elem2 = self._data[idx1], self._data[idx2]
         self._hashMap[elem1.val] = idx2
         self._hashMap[elem2.val] = idx1
@@ -45,13 +79,17 @@ class HashHeap(object):
     def _siftDown(self, idx):
         def heapValid(idx):
             left, right = idx * 2 + 1, idx * 2 + 2
-            if left > self._maxIdx: return True
-            if right > self._maxIdx: return self._data[idx] <= self._data[left]
+            if left > self._maxIdx:
+                return True
+            if right > self._maxIdx:
+                return self._data[idx] <= self._data[left]
             return self._data[idx] <= self._data[left] and self._data[idx] <= self._data[right]
         def smallerChild(idx):
             left, right = idx * 2 + 1, idx * 2 + 2
-            if left > self._maxIdx: return None
-            if right > self._maxIdx: return left
+            if left > self._maxIdx:
+                return None
+            if right > self._maxIdx:
+                return left
             return left if self._data[left] < self._data[right] else right
 
         current = idx
@@ -86,15 +124,31 @@ class HashHeap(object):
             self._siftDown(idx)
         return retVal
 
+    @property
+    def length(self):
+        """
+        Return the number of elements in the Hash Heap
+        """
+        return self._cap
+
     def heapPeep(self):
+        """
+        Return the MIN element in the Hash Heap
+        """
         if not self._data:
             return float("inf")
         return self._data[0].val
 
     def heapPop(self):
+        """
+        Remove the MIN element from the Hash Heap and return its value
+        """
         return self._removeByIdx(0)
 
     def heapPush(self, elem):
+        """
+        Push a new element into the Hash Heap
+        """
         self._cap += 1
         if elem not in self._hashMap:
             self._maxIdx += 1
@@ -106,6 +160,10 @@ class HashHeap(object):
             self._data[idx].cnt += 1
         
     def heapRemove(self, elem):
+        """
+        Remove a existing element from the Hash Heap
+        If the element to be removed is not in the Hash Heap, raise an error.
+        """
         if elem not in self._hashMap:
             raise ValueError("Element to be removed is not in HashHeap!!!")
         idx = self._hashMap[elem]
@@ -113,8 +171,10 @@ class HashHeap(object):
 
     def __contains__(self, value):
         return value in self._hashMap
+
     def __str__(self):
         return "%s" % [elem.val for elem in self._data]
+    __repr__ = __str__
 
 
 def pushpopTest():
@@ -133,13 +193,13 @@ def pushpopTest():
             thisHeap.heapPush(thisRandom)
             testList.append(thisRandom)
             assert min(testList) == thisHeap.heapPeep()
-            assert len(testList) == thisHeap._cap
+            assert len(testList) == thisHeap.length
             assert len(thisHeap._hashMap) == thisHeap._maxIdx + 1
         testList.sort()
-        assert len(testList) == thisHeap._cap
+        assert len(testList) == thisHeap.length
         for idx, num in enumerate(testList):
             assert num == thisHeap.heapPop()
-            assert len(testList) - 1 - idx == thisHeap._cap
+            assert len(testList) - 1 - idx == thisHeap.length
             assert len(thisHeap._hashMap) == thisHeap._maxIdx + 1
 
 def removeTest():
@@ -160,16 +220,16 @@ def removeTest():
             else:
                 testList.append(thisRandom)
             assert min(testList) == thisHeap.heapPeep()
-            assert len(testList) == thisHeap._cap
+            assert len(testList) == thisHeap.length
             assert len(thisHeap._hashMap) == thisHeap._maxIdx + 1
         testList.sort()
-        assert len(testList) == thisHeap._cap
+        assert len(testList) == thisHeap.length
         for idx, num in enumerate(testList):
             assert num == thisHeap.heapPop()
-            assert len(testList) - 1 - idx == thisHeap._cap
+            assert len(testList) - 1 - idx == thisHeap.length
             assert len(thisHeap._hashMap) == thisHeap._maxIdx + 1
+
 
 if __name__ == '__main__':
     pushpopTest()
     removeTest()
-
